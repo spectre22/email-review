497b29b95fe20a0bf973c1f0f438a061f30c79e8
Shubham Kansal <kansalshubham22@gmail.com>
2018-04-15 22:59:35 +0530

    origin

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express =require("express");
-const passport=require('passport');
-const GoogleStrategy=require('passport-google-oauth-20').Strategy;
+
 const app = express();
-passport.use(new GoogleStrategy());
 app.get('/',(req,res)=>
 {
    res.send({hi:'there'}); 
 });
 const PORT=process.env.PORT||5000
 app.listen(PORT);
-//client id1095398301831-v09pnbia5bmjlooqp1eucjj4h7rhuhn9.apps.googleusercontent.com
-//client secret:oW5gYvGEkLYvFsmLpOMYVq15
