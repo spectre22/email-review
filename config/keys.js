34c8a11f5b5b75039b3f41351663293a75f7dfd3
Shubham Kansal <kansalshubham22@gmail.com>
2018-04-19 23:55:49 +0530

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+client_ID:'1030481591148-43us19cu5m2er02psfbldodtpt6i85rq.apps.googleusercontent.com',
+client_SECRET:'Z-QMUS9VlL6474-zt1X11GRw',
+mongoURI:'mongodb://shubham:password@ds249079.mlab.com:49079/surveys-payments',
+cookieKey:'ttvgvvfxcegvvfcdxfcghv'
+};
