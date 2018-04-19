ead860c0c53234ae69ef728bc2eca9f9f5ed66f8
Shubham Kansal <kansalshubham22@gmail.com>
2018-04-20 00:51:25 +0530

    finished auth deployments

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,10 @@
-module.exports={
-client_ID:'1030481591148-43us19cu5m2er02psfbldodtpt6i85rq.apps.googleusercontent.com',
-client_SECRET:'Z-QMUS9VlL6474-zt1X11GRw',
-mongoURI:'mongodb://shubham:password@ds249079.mlab.com:49079/surveys-payments',
-cookieKey:'ttvgvvfxcegvvfcdxfcghv'
-};
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod set of keys
+    module.exports = require('./prod');
+  } else {
+    // we are in development - return the dev keys!!!
+    module.exports = require('./dev');
+  }
+
+
+
